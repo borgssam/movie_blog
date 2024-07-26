1388a14025220329fbb42eeb2cc4cb37429b06ad
borgssam <borgssam@gmail.com>
2024-07-26 12:13:16 +0900

    step01 폴더만들기

---
+++ src/step01/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {useState, usetEffect} from 'react';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        123
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/step03/App.js
@@ -0,0 +1,14 @@
+import left from './left.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={left} className="App-logo" alt="left" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
