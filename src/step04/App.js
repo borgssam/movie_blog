9b16dc154c499d5a341deb5a3b27d0677db85047
borgssam <borgssam@gmail.com>
2024-07-26 14:25:08 +0900

    step04 구조변경(Home)

--- src/step04/App.js
+++ src/step04/App.js
@@ -1,14 +1,17 @@
-import left from './left.svg';
+//2.영화 API읽기
+//npm install axios
+// 목록주소
+// https://yts.mx/api/v2/list_movies.json
+// https://yts-proxy.now.sh/list_movies.json
+// 상세보기주소
+// https://yts.mx/api/v2/movie_details.json?movie_id=11
+// https://yts-proxy.now.sh/movie_detail.json?movie_id=11
+import React from 'react';
+import Home from './routes/Home';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={left} className="App-logo" alt="left" />
-      </header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
